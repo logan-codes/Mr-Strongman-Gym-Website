a726e3c12539945f9fe39e07d3181763fa23283c
logan-codes <loganandamuthu2005@gmail.com>
2026-02-07 16:43:36 +0530

    patch: hero image

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,13 @@ import type { NextConfig } from "next";
 import path from "node:path";
 
 
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = '/Mr-Strongman-Gym-Website'; // Change this to your actual repo name
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: isProd ? repoName : '',
+  assetPrefix: isProd ? repoName : '',
   images: {
     unoptimized: true,
     remotePatterns: [
