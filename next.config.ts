0d507a4dc2cf19ec88bcffed976f5ec2127ee9fd
Loganandamuthu <loganandamuthu2005@gmail.com>
2026-03-10 16:18:34 +0530

    switching deployment from gh-pages to vercel

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from "next";
-import path from "node:path";
-
-
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = '/Mr-Strongman-Gym-Website'; // Change this to your actual repo name
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: isProd ? repoName : '',
-  assetPrefix: isProd ? repoName : '',
   images: {
     unoptimized: true,
     remotePatterns: [
@@ -22,7 +14,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  outputFileTracingRoot: path.resolve(__dirname, '../../'),
 };
 
 export default nextConfig;
